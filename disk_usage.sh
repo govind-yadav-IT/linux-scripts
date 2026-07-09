1bcbb6f34f9b28b017d22679b5f1aedf5a702d82
Govind <govindyadav8245@gmail.com>
2026-07-09 17:54:38 +0000

    Initial commit with Bash scripts and GitHub Actions workflow

---
+++ disk_usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Report Generated: $(date)"
+echo "Disk Usage Report"
+echo "_________________"
+df -h | grep -v tmpfs
+echo ""
+echo "Checking for partitions above 80%...."
+df -h | grep -v tmpfs | awk 'NR>1 {print $5, $6}' | while read usage mount; do
+     usage_num=${usage%?}
+     if [ "$usage_num" -gt 80 ]; then
+        echo "WARNING: $mount is at $usage - above 80%!"
+     else
+        echo "OK: $mount is at $usage"
+     fi
+done
+
